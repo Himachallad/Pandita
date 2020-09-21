cebf2630fb1dd580e08dd73d7e2da4f2b7e4339f
pra <pra@adobe.com>
2020-09-21 11:37:53 +0530

    changes for default port and URI

--- DB/connection.js
+++ DB/connection.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://pra:pragya3122$@cluster0.sies6.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  console.log('DB connected');
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('DB connected');
+  } catch (err) {
+    console.log('Error in connecting to DB' + err);
+    throw (err);
+  };
 };
 
 module.exports = connectDB;
-
--- blog-server/app.js
+++ blog-server/app.js
@@ -1,15 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const connectDB = require('../DB/connection');
 const dotenv = require('dotenv').config();
 
+const port = process.env.PORT || 8080;
 connectDB();
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome');
 
 });
 
-app.listen(process.env.PORT,function(){
+app.listen(port, function () {
     console.log('server started !');
 });
-
